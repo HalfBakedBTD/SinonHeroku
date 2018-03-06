fe584c9016d9d246aff7c4d80ae5a2e4b0db0bc7
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-06 15:49:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
@@ -30,11 +31,12 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
+  let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   
-  let commandfile = bot.commands.get(cmd.slice(2));
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
 });
 
