7ecb3b6455e41d21dbed0694c6d56a6cf16c2e7a
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-07 22:41:07 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 	message.channel.send("I have sent you a DM.")
-    return message.author.send("**All Commands:** Thanks <@275831434772742144> for making the website!\n\t :link: https://sinon-bot.github.io/commands.html :link:\n\n**Bot's Server:** Bot made by <@346687165868015616>! \n\t :link: https://discord.gg/tQqgqdt :link:")
+    return message.author.send("**All Commands:**\t\tThanks <@275831434772742144> for making the website!\n\t :link: https://sinon-bot.github.io/commands.html :link:\n\n**Bot's Server:**\t\tBot made by <@346687165868015616>! \n\t :link: https://discord.gg/tQqgqdt :link:")
 }
 module.exports.help = {
   name: "help"
